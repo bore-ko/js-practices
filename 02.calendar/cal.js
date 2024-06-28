12d65f3661cf41a827165f7064eb1df735f18173
bore-ko <tori.un.sourire@gmail.com>
2024-06-28 21:54:36 +0900

    1つのファイルにまとめる

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,5 +1,53 @@
 #!/usr/bin/env node
 
-import { flipCalendar } from "./calendar.js";
+import minimist from "minimist";
+
+function calcFirstDate() {
+  const argv = minimist(process.argv.slice(2));
+  const date = new Date();
+
+  if (argv["y"] >= 1970 && argv["y"] <= 2100) {
+    date.setFullYear(argv.y);
+  }
+
+  if (argv["m"] >= 1 && argv["m"] <= 12) {
+    date.setMonth(argv.m - 1);
+  }
+
+  date.setDate(1);
+  return date;
+}
+
+function flipCalendar() {
+  const firstDate = calcFirstDate();
+  const year = firstDate.getFullYear();
+  const month = firstDate.getMonth() + 1;
+  const lastDate = new Date(year, month, 0);
+  let formattedDays = "";
+
+  console.log(`${month}月 ${year}`.padStart(13, " "));
+  console.log("日", "月", "火", "水", "木", "金", "土");
+
+  const add_spaces_to_fit_start_day = " ".repeat(firstDate.getDay() * 3, " ");
+  formattedDays += add_spaces_to_fit_start_day;
+
+  for (let day = 1; day <= lastDate.getDate(); day++) {
+    const currentDate = new Date();
+    currentDate.setFullYear(year, month - 1, day);
+
+    let add_spaces_before_and_after_day = day.toString().padStart(2, " ");
+    if (currentDate.getDay() !== 6 && day !== lastDate.getDate()) {
+      add_spaces_before_and_after_day += " ";
+    }
+
+    formattedDays +=
+      currentDate.getDay() === 6
+        ? `${add_spaces_before_and_after_day}\n`
+        : add_spaces_before_and_after_day;
+  }
+  formattedDays += "\n";
+
+  process.stdout.write(formattedDays);
+}
 
 flipCalendar();
