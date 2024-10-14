254d67294744c9f74aee949a4b70129226d4543b
bore-ko <tori.un.sourire@gmail.com>
2024-10-14 20:58:57 +0900

    finallyとエラーcatchの修正

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -4,32 +4,40 @@ import timers from "timers/promises";
 import { initDb, run, get, all, closeDb } from "./promise.js";
 
 // async / await エラーなし
-initDb();
-await run(
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-);
-await run("INSERT INTO books (title) VALUES ('async/await 学習')");
-const row = await get("SELECT id FROM books");
-console.log(`id: ${row.id}`);
-const rows = await all("SELECT * FROM books");
-rows.forEach((row) => console.log(`id: ${row.id}, title: ${row.title}`));
-closeDb();
+try {
+  initDb();
+  await run(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+  await run("INSERT INTO books (title) VALUES ('async/await 学習')");
+  const row = await get("SELECT id FROM books");
+  console.log(`id: ${row.id}`);
+  const rows = await all("SELECT * FROM books");
+  rows.forEach((row) => console.log(`id: ${row.id}, title: ${row.title}`));
+} finally {
+  closeDb();
+}
 
 await timers.setTimeout(100);
 
 // async / await エラーあり
 initDb();
+await run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 try {
-  await run(
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
   await run("INSERT INTO notes (title) VALUES ('async/await 学習')");
 } catch (err) {
-  console.error(err.message);
+  if (err.code == "SQLITE_ERROR") {
+    console.error(err.message);
+  }
 }
 try {
   await all("SELECT * FROM memos");
 } catch (err) {
-  console.error(err.message);
+  if (err.code == "SQLITE_ERROR") {
+    console.error(err.message);
+  }
+} finally {
+  closeDb();
 }
-closeDb();
