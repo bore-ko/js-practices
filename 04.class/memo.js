d52441e59f98c6eca9b405837241675f85b6233d
bore-ko <tori.un.sourire@gmail.com>
2025-02-20 00:43:41 +0900

    クラス名の修正、非同期処理とエラー対応、参照での余分な空白の修正

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -2,109 +2,110 @@
 
 import readline from "readline";
 import enquirer from "enquirer";
-import { FileOperation } from "./file_operation.js";
+import { MemoManager } from "./memo_manager.js";
 
 class MemoApp {
   #argv;
   #file_location;
-  #fileOperation;
+  #memoManager;
 
   constructor() {
     this.#argv = process.argv[2];
     this.#file_location = "memos.json";
-    this.#fileOperation = new FileOperation();
-  }
-
-  #readLines(rl) {
-    return new Promise((resolve, reject) => {
-      const lines = [];
-
-      rl.on("line", (line) => {
-        lines.push(line);
-      });
-
-      rl.on("close", () => {
-        if (lines.length === 0) {
-          resolve(false);
-        } else {
-          resolve(lines);
-        }
-      });
-
-      rl.on("error", (err) => {
-        reject(err);
-      });
-    });
+    this.#memoManager = new MemoManager();
   }
 
   async #list() {
     try {
-      if (await this.#fileOperation.isAccess(this.#file_location)) {
-        const file_location = await this.#fileOperation.read(
-          this.#file_location,
-        );
-        const memos = JSON.parse(file_location);
-        const firstLines = memos.map((memo) => memo.lines[0]);
-        console.log(firstLines.join("\n"));
-      } else {
-        (err) => {
-          console.err(err);
-          throw err;
-        };
+      const isAccess = await this.#memoManager.isAccess(this.#file_location);
+      if (!isAccess) {
+        console.log("There are no memo.");
+        return;
       }
+
+      const file_location = await this.#memoManager.read(this.#file_location);
+      const memos = JSON.parse(file_location);
+      if (memos.length === 0) {
+        console.log("There are no memo.");
+        return;
+      }
+
+      const firstLines = memos.map((memo) => memo.lines[0]);
+      console.log(firstLines.join("\n"));
     } catch (err) {
       console.error(err);
+      throw err;
     }
   }
 
   async #reference() {
-    const file_location = await this.#fileOperation.read(this.#file_location);
-    const memos = JSON.parse(file_location);
-    if (memos.length === 0) {
-      return;
-    }
-    const prompt = new enquirer.Select({
-      name: "memo",
-      message: "Choose a memo you want to see:",
-      footer() {
-        const index = this.index;
-        const lines = String(memos[index].lines).replace(/,/g, "\n");
-        return `\n${lines}`;
-      },
-
-      choices: memos.map((memo) => memo.lines[0]),
-    });
     try {
+      const isAccess = await this.#memoManager.isAccess(this.#file_location);
+      if (!isAccess) {
+        console.log("There are no memo.");
+        return;
+      }
+
+      const file_location = await this.#memoManager.read(this.#file_location);
+      const memos = JSON.parse(file_location);
+      if (memos.length === 0) {
+        console.log("There are no memo.");
+        return;
+      }
+
+      const prompt = new enquirer.Select({
+        name: "memo",
+        message: "Choose a memo you want to see:",
+        footer() {
+          const index = this.index;
+          const lines = memos[index].lines
+            .filter((line) => line !== "")
+            .join("\n");
+          return `\n${lines}`;
+        },
+
+        choices: memos.map((memo) => memo.lines[0]),
+      });
       await prompt.run();
     } catch (err) {
       console.error(err);
+      throw err;
     }
   }
 
   async #delete() {
-    const file_location = await this.#fileOperation.read(this.#file_location);
-    const memos = JSON.parse(file_location);
-    if (memos.length === 0) {
-      return;
-    }
-    const prompt = new enquirer.Select({
-      name: "memo",
-      message: "Choose a memo you want to delete:",
-      choices: memos.map((memo) => memo.lines[0]),
-      result() {
-        return this.index + 1;
-      },
-    });
-    prompt
-      .run()
-      .then((result) => {
-        const index = result - 1;
-        memos.splice(index, 1);
+    try {
+      const isAccess = await this.#memoManager.isAccess(this.#file_location);
+      if (!isAccess) {
+        console.log("There are no memo.");
+        return;
+      }
 
-        const jsonMemos = JSON.stringify(memos, null, "\t");
-        this.#fileOperation.write(this.#file_location, jsonMemos);
-      })
-      .catch(console.error);
+      const file_location = await this.#memoManager.read(this.#file_location);
+      const memos = JSON.parse(file_location);
+      if (memos.length === 0) {
+        console.log("There are no memo.");
+        return;
+      }
+
+      const prompt = new enquirer.Select({
+        name: "memo",
+        message: "Choose a memo you want to delete:",
+        choices: memos.map((memo) => memo.lines[0]),
+        result() {
+          return this.index + 1;
+        },
+      });
+      const deleteMemo = await prompt.run();
+      const index = deleteMemo - 1;
+      memos.splice(index, 1);
+
+      const jsonMemos = JSON.stringify(memos, null, "  ");
+      await this.#memoManager.write(this.#file_location, jsonMemos);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
 
   async #add() {
@@ -113,23 +114,22 @@ class MemoApp {
     });
 
     try {
-      const inputLines = await this.#readLines(rl);
+      const inputLines = await this.#memoManager.readLines(rl);
       rl.close();
 
-      const lines = await this.#fileOperation.isAccess(this.#file_location);
+      const isAccess = await this.#memoManager.isAccess(this.#file_location);
       let memos = [];
-      if (lines) {
-        const file_content = await this.#fileOperation.read(
-          this.#file_location,
-        );
-        memos = JSON.parse(file_content);
+      if (isAccess) {
+        const file_location = await this.#memoManager.read(this.#file_location);
+        memos = JSON.parse(file_location);
       }
 
       memos.push({ lines: inputLines });
       const jsonMemos = JSON.stringify(memos, null, "  ");
-      await this.#fileOperation.write(this.#file_location, jsonMemos);
+      await this.#memoManager.write(this.#file_location, jsonMemos);
     } catch (err) {
       console.error(err);
+      throw err;
     }
   }
 
--- 04.class/memo_manager.js
+++ 04.class/memo_manager.js
@@ -1,6 +1,6 @@
 import fs from "node:fs/promises";
 
-export class FileOperation {
+export class MemoManager {
   async isAccess(file_location) {
     try {
       await fs.access(file_location);
@@ -30,6 +30,28 @@ export class FileOperation {
       console.error(err);
     }
   }
+
+  readLines(rl) {
+    return new Promise((resolve, reject) => {
+      const lines = [];
+
+      rl.on("line", (line) => {
+        lines.push(line);
+      });
+
+      rl.on("close", () => {
+        if (lines.length === 0) {
+          resolve(false);
+        } else {
+          resolve(lines);
+        }
+      });
+
+      rl.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
 }
 
-new FileOperation();
+new MemoManager();
